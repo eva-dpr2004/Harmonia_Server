2f998e3cf11b399fd34097fb41838ac783715c2c
unknown <rosarioeva3@gmail.com>
2024-08-27 19:21:21 +0200

    Check-If-Duplicate-Activity-And-Monthly-Delete-All-Activities

--- controllers/Activities.js
+++ controllers/Activities.js
@@ -1,12 +1,13 @@
 const { Op } = require('sequelize'); 
 const { Activites, Animaux } = require('../models');
+const cron = require('node-cron');
 
-const ajoutActivite = async (req, res) => {
+  const ajoutActivite = async (req, res) => {
   const { animalId, date, debutActivite, finActivite } = req.body;
 
   const debutDate = new Date(`1970-01-01T${debutActivite}Z`);
   let finDate = new Date(`1970-01-01T${finActivite}Z`);
-  
+
   if (finDate < debutDate) {
     finDate.setDate(finDate.getDate() + 1);
   }
@@ -22,6 +23,21 @@ const ajoutActivite = async (req, res) => {
       return res.status(404).json({ error: "Animal not found" });
     }
 
+    // Vérifiez si une activité avec les mêmes heures existe déjà pour cet animal à cette date
+    const existingActivity = await Activites.findOne({
+      where: {
+        Id_Animal: animalId,
+        Date: date,
+        Debut_Activite: debutActivite,
+        Fin_Activite: finActivite
+      },
+    });
+
+    if (existingActivity) {
+      return res.status(400).json({ error: 'Une activité avec ces heures existe déjà pour cet animal à cette date.' });
+    }
+
+    // Limite d'activités par jour
     const activitiesCount = await Activites.count({
       where: {
         Id_Animal: animalId,
@@ -29,15 +45,16 @@ const ajoutActivite = async (req, res) => {
       }
     });
 
-    const maxActivitiesPerDay = 10; 
+    const maxActivitiesPerDay = 10;
 
     if (activitiesCount >= maxActivitiesPerDay) {
       return res.status(400).json({ error: `Limite d'activités atteinte pour cet animal aujourd'hui.` });
     }
 
+    // Créez l'activité si aucune activité identique n'existe
     const newActivity = await Activites.create({
       Id_Animal: animalId,
-      Nom_Animal: animal.Nom, // Inclure le nom de l'animal
+      Nom_Animal: animal.Nom,
       Date: date,
       Debut_Activite: debutActivite,
       Fin_Activite: finActivite,
@@ -89,5 +106,20 @@ const deleteActivitiesById = async (req, res) => {
     res.status(500).json({ error: 'Erreur interne du serveur', details: error.message });
   }
 };
+
+// Suppression de toutes les activités tout les 1er du mois
+const deleteAllActivities = async () => {
+  try {
+    await Activites.destroy({ where: {} });
+    console.log('Toutes les activités ont été supprimées.');
+  } catch (error) {
+    console.error('Erreur lors de la suppression des activités:', error);
+  }
+};
+
+cron.schedule('0 0 1 * *', async () => {
+  console.log('Début de la suppression des activités - 1er du mois');
+  await deleteAllActivities();
+});
 
 module.exports = { ajoutActivite, getActivitesByUserId, deleteActivitiesById };
--- controllers/Users.js
+++ controllers/Users.js
@@ -1,32 +1,50 @@
 const { Utilisateurs } = require("../models");
-const bcrypt = require("bcrypt");
+//const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const { sign } = require('jsonwebtoken');
+const validator = require('validator');
 const { Op } = require("sequelize");
 
 //Inscription
 const createUser = async (req, res) => {
+  // Extraire les données de la requête
   const { Nom, Email, Mot_De_Passe } = req.body;
+
+  const sanitizedNom = validator.escape(Nom.trim());
+  const sanitizedEmail = validator.normalizeEmail(Email.trim());
+  
+  if (!validator.isLength(sanitizedNom, { min: 3, max: 15 }) ||
+      !validator.isAlphanumeric(sanitizedNom.replace(/[^a-zA-Z0-9]/g, '')) ||
+      !validator.isEmail(sanitizedEmail) ||
+      !validator.isLength(Mot_De_Passe, { min: 12 })) {
+    return res.status(400).json({ error: "Données d'entrée invalides" });
+  }
+
   try {
-      const existingUserByName = await Utilisateurs.findOne({ where: { Nom } });
-      if (existingUserByName) {
-          return res.status(400).json({ error: "Nom déjà pris" });
-      }
+    // Vérifier si le nom ou l'email existent déjà
+    const existingUserByName = await Utilisateurs.findOne({ where: { Nom: sanitizedNom } });
+    if (existingUserByName) {
+      return res.status(400).json({ error: "Nom déjà pris" });
+    }
 
-      const existingUserByEmail = await Utilisateurs.findOne({ where: { Email } });
-      if (existingUserByEmail) {
-          return res.status(400).json({ error: "Email déjà pris" });
-      }
+    const existingUserByEmail = await Utilisateurs.findOne({ where: { Email: sanitizedEmail } });
+    if (existingUserByEmail) {
+      return res.status(400).json({ error: "Email déjà pris" });
+    }
 
-      const hash = await bcrypt.hash(Mot_De_Passe, 10);
-      const newUser = await Utilisateurs.create({
-          Nom,
-          Email,
-          Mot_De_Passe: hash,
-      });
-      res.json({ success: true, message: "Utilisateur créé avec succès", user: newUser });
+    const hash = await bcrypt.hash(Mot_De_Passe, 10);
+
+    const newUser = await Utilisateurs.create({
+      Nom: sanitizedNom,
+      Email: sanitizedEmail,
+      Mot_De_Passe: hash,
+    });
+
+    // Réponse de succès
+    res.json({ success: true, message: "Utilisateur créé avec succès", user: newUser });
   } catch (error) {
-      console.error("Erreur lors de la création de l'utilisateur:", error);
-      res.status(500).json({ error: "Erreur interne du serveur", details: error.message });
+    console.error("Erreur lors de la création de l'utilisateur:", error);
+    res.status(500).json({ error: "Erreur interne du serveur", details: error.message });
   }
 };
 
@@ -39,39 +57,42 @@ const LOCK_TIME = [5 * 60 * 1000, 15 * 60 * 1000, 30 * 60 * 1000];
 const loginUser = async (req, res) => {
   const { NomOrEmail, Mot_De_Passe } = req.body;
 
+  // Sanitisation et validation
+  const sanitizedNomOrEmail = validator.escape(NomOrEmail.trim());
+  const sanitizedMot_De_Passe = Mot_De_Passe.trim();
+  
+  // Validation des données
+  if (!validator.isLength(sanitizedNomOrEmail, { min: 3 }) || !validator.isLength(sanitizedMot_De_Passe, { min: 12 })) {
+    return res.status(400).json({ error: "Données d'entrée invalides" });
+  }
+
   try {
     let utilisateur;
 
-    if (NomOrEmail.includes('@')) {
-      utilisateur = await Utilisateurs.findOne({ where: { Email: NomOrEmail } });
+    if (validator.isEmail(sanitizedNomOrEmail)) {
+      utilisateur = await Utilisateurs.findOne({ where: { Email: sanitizedNomOrEmail } });
     } else {
-      utilisateur = await Utilisateurs.findOne({ where: { Nom: NomOrEmail } });
+      utilisateur = await Utilisateurs.findOne({ where: { Nom: sanitizedNomOrEmail } });
     }
 
     if (!utilisateur) {
-      handleFailedAttempt(NomOrEmail);
+      handleFailedAttempt(sanitizedNomOrEmail);
       return res.json({ error: "Utilisateur non existant" });
     }
 
-    if (isUserLocked(NomOrEmail)) {
-      const lockUntil = failedAttempts[NomOrEmail].lockUntil;
+    if (isUserLocked(sanitizedNomOrEmail)) {
+      const lockUntil = failedAttempts[sanitizedNomOrEmail].lockUntil;
       const lockTimeLeft = Math.round((lockUntil - Date.now()) / 1000); // in seconds
       return res.status(403).json({ error: `Compte verrouillé. Réessayez dans ${lockTimeLeft} secondes.` });
     }
 
-    const match = await bcrypt.compare(Mot_De_Passe, utilisateur.Mot_De_Passe);
+    const match = await bcrypt.compare(sanitizedMot_De_Passe, utilisateur.Mot_De_Passe);
     if (!match) {
-      handleFailedAttempt(NomOrEmail);
+      handleFailedAttempt(sanitizedNomOrEmail);
       return res.json({ error: "Mot de passe incorrect" });
     }
 
-    // Comparaison stricte pour Nom ou Email
-    if ((NomOrEmail.includes('@') && utilisateur.Email !== NomOrEmail) ||
-        (!NomOrEmail.includes('@') && utilisateur.Nom !== NomOrEmail)) {
-      return res.json({ error: "Utilisateur non existant" });
-    }
-
-    resetFailedAttempts(NomOrEmail);
+    resetFailedAttempts(sanitizedNomOrEmail);
     const accessToken = sign(
       { Nom: utilisateur.Nom, Id_Utilisateur: utilisateur.Id_Utilisateur }, 
       "importantsecret"
@@ -153,31 +174,40 @@ const getBasicInfo = async (req, res) => {
 
 //Modifier Utilisateur
 const updateUser = async (req, res) => {
-    const Id_Utilisateur = req.params.id;  
-    const { Nom, Email, Mot_De_Passe } = req.body;
+  const Id_Utilisateur = req.params.id;  
+  const { Nom, Email, Mot_De_Passe } = req.body;
 
-    try {
-        const utilisateur = await Utilisateurs.findByPk(Id_Utilisateur);
-        if (!utilisateur) {
-            return res.status(404).json({ error: "Utilisateur non trouvé" });
-        }
+  try {
+      const utilisateur = await Utilisateurs.findByPk(Id_Utilisateur);
+      if (!utilisateur) {
+          return res.status(404).json({ error: "Utilisateur non trouvé" });
+      }
 
-        // Hashage du nouveau mdp
-        if (Mot_De_Passe) {
-            utilisateur.Mot_De_Passe = await bcrypt.hash(Mot_De_Passe, 10);
-        }
+      let sanitizedNom = Nom ? validator.escape(Nom.trim()) : null;
+      let sanitizedEmail = Email ? validator.normalizeEmail(Email.trim()) : null;
 
-        // MAJ des champs nom et email s'ils sont fournis
-        if (Nom) utilisateur.Nom = Nom;
-        if (Email) utilisateur.Email = Email;
-        
-        await utilisateur.save();
+      if (sanitizedNom && !validator.isLength(sanitizedNom, { min: 3, max: 15 })) {
+          return res.status(400).json({ error: "Le nom doit contenir entre 3 et 15 caractères" });
+      }
+      
+      if (sanitizedEmail && !validator.isEmail(sanitizedEmail)) {
+          return res.status(400).json({ error: "L'email est invalide" });
+      }
 
-        res.json({ success: true, message: "Utilisateur mis à jour avec succès", utilisateur });
-    } catch (error) {
-        console.error("Erreur lors de la mise à jour de l'utilisateur:", error);
-        res.status(500).json({ error: "Erreur interne du serveur", details: error.message });
-    }
+      if (Mot_De_Passe) {
+          utilisateur.Mot_De_Passe = await bcrypt.hash(Mot_De_Passe, 10);
+      }
+
+      if (sanitizedNom) utilisateur.Nom = sanitizedNom;
+      if (sanitizedEmail) utilisateur.Email = sanitizedEmail;
+      
+      await utilisateur.save();
+
+      res.json({ success: true, message: "Utilisateur mis à jour avec succès", utilisateur });
+  } catch (error) {
+      console.error("Erreur lors de la mise à jour de l'utilisateur:", error);
+      res.status(500).json({ error: "Erreur interne du serveur", details: error.message });
+  }
 };
 
 //Supprimer Utilisateur
