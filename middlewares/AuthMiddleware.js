5d66f3eb1cc790a0d216dc144ff95f77ac6f6957
unknown <rosarioeva3@gmail.com>
2024-09-11 02:34:17 +0200

    Fix database connection for production

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,19 +1,30 @@
 const { verify } = require("jsonwebtoken");
 
 const validateToken = (req, res, next) => {
+  // Récupération du token d'authentification depuis les cookies
   const token = req.cookies.accessToken;
-  
-  if (!token) return res.json({ error: "Utilisateur non connecté" });
+
+  // Si aucun token n'est trouvé, renvoyer une réponse indiquant que l'utilisateur n'est pas connecté
+  if (!token) {
+    return res.status(403).json({ error: "Utilisateur non connecté" });
+  }
 
   try {
-    const validToken = verify(token, process.env.SECRET_KEY); 
-    req.utilisateur = validToken;
+    // Vérification du token avec la clé secrète
+    const validToken = verify(token, process.env.SECRET_KEY);
+    req.utilisateur = validToken;  // Ajouter les informations du token à la requête pour une utilisation future
+    
+    // Si le token est valide, passer à l'étape suivante du middleware
     if (validToken) {
       return next();
     }
   } catch (err) {
-    res.clearCookie('accessToken');
-    return res.status(401).json({ error: "Token expiré ou invalide. Veuillez vous reconnecter.", details: err.message });
+    // En cas d'erreur de vérification, supprimer le cookie et renvoyer une réponse d'erreur
+    res.clearCookie('accessToken', { path: '/' });
+    return res.status(401).json({
+      error: "Token expiré ou invalide. Veuillez vous reconnecter.",
+      details: err.message  // Pour des informations plus détaillées sur l'erreur
+    });
   }
 };
 
