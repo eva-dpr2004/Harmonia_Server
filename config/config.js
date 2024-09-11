25e24a3d5bdbe185f4b5c7b45bea5515e08fc389
unknown <rosarioeva3@gmail.com>
2024-09-11 03:17:30 +0200

    Fix database connection for production

--- config/config.js
+++ config/config.js
@@ -1,26 +1,43 @@
 module.exports = {
-    development: {
-      username: "root",
-      password: null,
-      database: "harmonia",
-      host: "localhost",
-      dialect: "mysql"
-    },
-    test: {
-      username: "root",
-      password: null,
-      database: "database_test",
-      host: "127.0.0.1",
-      dialect: "mysql"
-    },
-    production: {
-      use_env_variable: "CLEARDB_DATABASE_URL", // Utilisation de ClearDB sur Heroku
-      dialect: "mysql",
-      dialectOptions: {
-        ssl: {
-          rejectUnauthorized: false // Nécessaire pour ClearDB SSL
-        }
-      }
+  development: {
+    username: "root",
+    password: null,
+    database: "harmonia",
+    host: "localhost",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
     }
-  };
-  
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  },
+  production: {
+    use_env_variable: "CLEARDB_DATABASE_URL",
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false // Nécessaire pour ClearDB SSL
+      }
+    },
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+};
