fe4995b8767b3001cb51149e734c54bd3dacf54c
unknown <rosarioeva3@gmail.com>
2024-09-11 02:51:44 +0200

    Fix database connection for production

--- controllers/Users.js
+++ controllers/Users.js
@@ -111,7 +111,7 @@ const loginUser = async (req, res) => {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'none',
+      sameSite: 'None',
       maxAge: 60 * 60 * 1000  
     });
 
